6a1a2a95fa2ffb2d033434e79d0ac9cc7b3caa84
Hasankhan170 <hasankhanali0316@gmail.com>
2024-12-15 12:24:38 +0500

    yeah is working properly

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Register from './pages/Register.jsx'
+import Login from './pages/Login.jsx'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <App/>,
+    children : [
+      {
+        path : "/Register",
+        element : <Register/>
+      },
+      {
+        path : "/Login",
+        element : <Login/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}>
+    <App/>
+  </RouterProvider>
 )
